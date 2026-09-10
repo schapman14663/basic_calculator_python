313303652817b8705ed20167a1dd19a8fdedaa97
Simon Chapman <schapman14663@gmail.com>
2026-09-10 20:29:48 +0100

    argparse

---
+++ src/calculator/__init__.py
@@ -0,0 +1,25 @@
+import argparse
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+
+    print("Hello from basic-calculator!")
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+def subtract(x: int, y: int) -> int:
+    return x - y
+
+def product(x: int, y: int) -> int:
+    return x * y
+
+def quotient(x: int, y: int) -> int:
+    return x // y
+
+def to_power(x: int, y: int) -> int:
+    result = 1 
+    for i in range(y):
+        result *= x
+    return result
