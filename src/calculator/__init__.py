141b84ea9f4b5596303c2b40bd06a92b18a0ce9c
schapman14663 <schapman14663@gmail.com>
2026-09-13 19:02:38 +0100

    fixed adding function

--- src/calculator/__init__.py
+++ src/calculator/__init__.py
@@ -1,38 +1,47 @@
 import argparse
-
-def add(x: int, y: int) -> int:
-    return x + y
-
-def subtract(x: int, y: int) -> int:
-    return x - y
-
-def product(x: int, y: int) -> int:
-    return x * y
-
-def quotient(x: int, y: int) -> int:
-    return x // y
-
-def to_power(x: int, y: int) -> int:
-    result = 1 
-    for i in range(y):
-        result *= x
-    return result
+from os import wait
 
 
-def main() -> int:
+def main() -> None:
     parser = argparse.ArgumentParser(prog="Calculator", description="A basic integer calculator",)
-    parser.add_argument('a', 'add')
-    parser.add_argument('s', 'sub')
-    parser.add_argument('m', 'mul')
-    parser.add_argument('d', 'div')
+    parser.add_argument('-a', '--add', action="store_true")
+    parser.add_argument('-s', '--sub')
+    parser.add_argument('-m', '--mul')
+    parser.add_argument('-d', '--div')
+    parser.add_argument('x', type=int, help="The first number you want to use in your calculation")
+    parser.add_argument('y', type=int, help="The second number you want to use in your calculation")
     
     args = parser.parse_args()
 
    
-    if args.add:
-        return add(x, y)
- 
+
 
     print("Hello from basic-calculator!")
+    def add(x: int, y: int) -> int:
+        return args.x + args.y
+
+    def subtract(x: int, y: int) -> int:
+        return x - y
+
+    def product(x: int, y: int) -> int:
+        return x * y
+
+    def quotient(x: int, y: int) -> int:
+        return x // y
+
+    def to_power(x: int, y: int) -> int:
+        result = 1 
+        for i in range(y):
+            result *= x
+        return result
+
+    if args.add:
+        ans = add(args.x, args.y)
+        print(str(ans))
+        print(args)
+    
+    if args.sub:
+        return subtract(x, y)
+
 
 
